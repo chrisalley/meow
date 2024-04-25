da1687aad24d03d350d32f24b192b430dce24149
Chris Alley <chris@chrisalley.info>
2024-04-25 16:45:05 +1200

    Chapter 2: Sorting the Table

--- lib/meow_web/live/meerkat_live.ex
+++ lib/meow_web/live/meerkat_live.ex
@@ -1,16 +1,44 @@
 defmodule MeowWeb.MeerkatLive do
   use MeowWeb, :live_view
   alias Meow.Meerkats
+  alias MeowWeb.Forms.SortingForm
 
   @impl true
   def mount(_params, _session, socket), do: {:ok, socket}
 
   @impl true
-  def handle_params(_params, _url, socket) do
-    {:noreply, assign_meerkats(socket)}
+  def handle_params(params, _url, socket) do
+    socket =
+      socket
+      |> parse_params(params)
+      |> assign_meerkats()
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:update, opts}, socket) do
+    path = Routes.live_path(socket, __MODULE__, opts)
+    {:noreply, push_patch(socket, to: path, replace: true)}
+  end
+
+  defp parse_params(socket, params) do
+    with {:ok, sorting_opts} <- SortingForm.parse(params) do
+      assign_sorting(socket, sorting_opts)
+    else
+      _error ->
+        assign_sorting(socket)
+    end
+  end
+
+  defp assign_sorting(socket, overrides \\ %{}) do
+    opts = Map.merge(SortingForm.default_values(), overrides)
+    assign(socket, :sorting, opts)
   end
 
   defp assign_meerkats(socket) do
-    assign(socket, :meerkats, Meerkats.list_meerkats())
+    %{sorting: sorting} = socket.assigns
+
+    assign(socket, :meerkats, Meerkats.list_meerkats(sorting))
   end
 end
