587658911433fbfec39c885239dcc77b537fcf07
Chris Alley <chris@chrisalley.info>
2024-05-18 20:19:55 +1200

    Chapter 3: Filtering the Table

--- lib/meow_web/live/meerkat_live.ex
+++ lib/meow_web/live/meerkat_live.ex
@@ -2,6 +2,7 @@ defmodule MeowWeb.MeerkatLive do
   use MeowWeb, :live_view
   alias Meow.Meerkats
   alias MeowWeb.Forms.SortingForm
+  alias MeowWeb.Forms.FilterForm
 
   @impl true
   def mount(_params, _session, socket), do: {:ok, socket}
@@ -18,16 +19,22 @@ defmodule MeowWeb.MeerkatLive do
 
   @impl true
   def handle_info({:update, opts}, socket) do
-    path = Routes.live_path(socket, __MODULE__, opts)
+    params = merge_and_sanitize_params(socket, opts)
+    path = Routes.live_path(socket, __MODULE__, params)
     {:noreply, push_patch(socket, to: path, replace: true)}
   end
 
   defp parse_params(socket, params) do
-    with {:ok, sorting_opts} <- SortingForm.parse(params) do
-      assign_sorting(socket, sorting_opts)
+    with {:ok, sorting_opts} <- SortingForm.parse(params),
+         {:ok, filter_opts} <- FilterForm.parse(params) do
+      socket
+      |> assign_filter(filter_opts)
+      |> assign_sorting(sorting_opts)
     else
       _error ->
-        assign_sorting(socket)
+        socket
+        |> assign_sorting()
+        |> assign_filter()
     end
   end
 
@@ -36,9 +43,24 @@ defmodule MeowWeb.MeerkatLive do
     assign(socket, :sorting, opts)
   end
 
-  defp assign_meerkats(socket) do
-    %{sorting: sorting} = socket.assigns
+  defp assign_filter(socket, overrides \\ %{}) do
+    assign(socket, :filter, FilterForm.default_values(overrides))
+  end
 
-    assign(socket, :meerkats, Meerkats.list_meerkats(sorting))
+  defp assign_meerkats(socket) do
+    params = merge_and_sanitize_params(socket)
+
+    assign(socket, :meerkats, Meerkats.list_meerkats(params))
+  end
+
+  defp merge_and_sanitize_params(socket, overrides \\ %{}) do
+    %{sorting: sorting, filter: filter} = socket.assigns
+
+    %{}
+    |> Map.merge(sorting)
+    |> Map.merge(filter)
+    |> Map.merge(overrides)
+    |> Enum.reject(fn {_key, value} -> is_nil(value) end)
+    |> Map.new()
   end
 end
